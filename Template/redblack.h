1798222387d08f38fc9f4a25e94aa39c81eceadb
vep20 <vep20@inf.ufpr.br>
2025-12-23 14:40:09 -0300

    Commit automático

---
+++ Template/redblack.h
@@ -0,0 +1,32 @@
+#ifndef REDBLACK_H_
+#define REDBLACK_H_
+
+struct nodo {
+    int chave;
+    ???
+};
+
+struct aluno{
+    char* nome;//nome completo
+    char* nomeDinf;//nome de usuário nas servidoras do dinf
+    unsigned int grr;
+};
+
+struct aluno* getAluno();
+
+void imprimirDadosAluno();
+
+//retorna SENTINELA se não foi possível inserir
+struct nodo* inserir(struct nodo** raiz, int chave);
+
+// retorna o número de nodos excluídos
+int excluir(struct nodo** raiz, int chave);
+
+//retorna SENTINELA se não existe
+struct nodo* buscar(struct nodo* raiz, int chave);
+
+void imprimirEmOrdem(struct nodo* nodo);
+
+void imprimirEmLargura(struct nodo* raiz);
+
+#endif//REDBLACK_H
