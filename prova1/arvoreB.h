49554b0e0f2803e70da87d6f66c23aae06b42bac
vep20 <vep20@inf.ufpr.br>
2026-04-05 17:02:32 -0300

    Commit automático

--- prova1/arvoreB.h
+++ prova1/arvoreB.h
@@ -19,9 +19,13 @@ struct arvoreB {
 };
 
 struct arvoreB* criarArvoreB(int32_t t_arvore);
+
 void inserirArvoreB(struct arvoreB* arvore, int32_t chave);
+
 void imprimirArvoreB(struct arvoreB* arvore);
+
 void imprimirEmOrdem(struct arvoreB* arvore);
+
 //union ou 
 struct nodo* buscarArvoreB(struct arvoreB* arvore, int32_t chave,
                             int32_t* idxEncontrado);
